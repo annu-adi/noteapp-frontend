0ed339e45cb5fc8d327f5b4cdb540744ebd47673
annu-adi <rashmi.maurya10@gmail.com>
2025-05-22 19:53:18 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     const handleSubmit = async (e) =>{
         e.preventDefault();
         try{
-       const response = await axios.post("http://localhost:5000/api/auth/login",{ email, password}
+       const response = await axios.post("https://noteapp-backend-rho.vercel.app/api/auth/login",{ email, password}
 
        );
        if(response.data.success){
