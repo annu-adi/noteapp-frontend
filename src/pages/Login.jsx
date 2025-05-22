834713a842175e0a689a7409eb3bc8d87f15cea6
annu-adi <rashmi.maurya10@gmail.com>
2025-05-22 19:27:37 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
     const handleSubmit = async (e) =>{
         e.preventDefault();
         try{
-       const response = await axios.post("https://vercel.com/rashmis-projects-3cacc26c/noteapp-frontend-7b1h/api/auth/login",{ email, password}
+       const response = await axios.post("http://localhost:5000/api/auth/login",{ email, password}
 
        );
        if(response.data.success){
